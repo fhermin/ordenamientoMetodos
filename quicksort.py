4050031bd8032965979bc3455c5f26a2fbd1da5e
Fhermin <fhermin.alex@gmail.com>
2024-07-02 23:20:28 -0700

    Ya terminado cada algoritmo de ordenamiento

--- quicksort.py
+++ quicksort.py
@@ -1,30 +1,22 @@
-def quick_sort (array, low , high) :
-    if low < high :
-        aux = particion(array, low , high)
+def quick_sort(array, low, high):
+    if low < high:
+        aux = partition(array, low, high)
+        quick_sort(array, low, aux - 1)
+        quick_sort(array, aux + 1, high)
 
-
-        quick_sort(array , low , aux -1)
-        quick_sort(array , aux + 1 , high)
-
-def particion(array, low , high):
-
-    pivote = array[high]
-
+def partition(array, low, high):
+    pivot = array[high]
     i = low - 1
-
-    for j in range(low,high) :
-
-        if array[j] <= pivote:
-            i =  i + 1
-            (array[i],array[j]) = (array[j],array[i])
-    
-    (array[i+1],array[j]) = (array[high],array[i+1])
-
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[high] = array[high], array[i + 1]
     return i + 1
 
-
-scores = [56,12,87,15,68,2,78,45]
-print("Antes de ordenarse : ")
+scores = [56, 12, 87, 15, 68, 2, 78, 45]
+print("Antes de ordenarse:")
 print(scores)
-print("Despues de ordenarse : ")
-print(quick_sort(scores))
+quick_sort(scores, 0, len(scores) - 1)
+print("Despues de ordenarse:")
+print(scores)
